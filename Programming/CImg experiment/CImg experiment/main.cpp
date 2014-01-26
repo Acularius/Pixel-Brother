2f175c0f2347b48a75797b3530d880b59559d3e3
Acularius <smithdonaldt@gmail.com>
2014-01-25 20:18:12 -0500

    Joys of Vectors.
    -2nd stage of the concept.
    Just learning the joys of vectors.
    A matrix that can be dynamic in terms of its size, and can read said
    size from the variables you put upon it.

--- Programming/CImg experiment/CImg experiment/main.cpp
+++ Programming/CImg experiment/CImg experiment/main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
+#include <vector>
 #include "CImg.h"
-using namespace cimg_library;
+// using namespace cimg_library;
 int main()
 {
 
 
-CImg<unsigned char> img("images/splotches.bmp"); // Reads from .bmp files natively
+cimg_library::CImg<unsigned char> img("images/splotches.bmp"); // Reads from .bmp files natively
 
-int width = img.width();
-int height = img.height();
+int width = img.width(); // X coords to create an array
+int height = img.height(); //Y coords to create an array
 float red = img(99,99,0, 0); // Red Channel
 float green = img(99,99,0, 1); // Green Channel
 float blue = img(99,99,0, 2); // Blue Channel
 int numChannels = img.spectrum(); // Number of Channels
+    // Create
+    std::vector< std::vector<char> > redArray(height, std::vector<char>(width));
+    // Write
+	char testWrite;
+//    redArray[2][3] = testWrite;
+    // Read
+    char testRead;
+
+
+
+
 
 std::cout << "The file is  " << width << " pixels wide" << std::endl;
 std::cout << "The file is  " << height << " pixels high" << std::endl;
@@ -20,6 +32,12 @@ std::cout << "Number of Channels: " << numChannels << std::endl;
 std::cout << "This much Red: " << red << std::endl;
 std::cout << "This much Green: " << green << std::endl;
 std::cout << "This much Blue: " << blue << std::endl;
+std::cout << "Testing input for testWrite variable" << std::endl;
+std::cin >> testWrite; std::cout << std::endl;
+redArray[199][199] = testWrite;
+testRead = redArray[199][199];
+std::cout << "The value recieved for testWrite was: " << testRead << std::endl;
+
 
 system("pause");
 return 0;
