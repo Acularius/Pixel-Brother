d43adf063230e07c9196727491f47639c0fd0ea6
SoapWarrior <aaron-metallion@hotmail.com>
2014-02-25 07:10:57 -0500

    Aaron Prototype 1.2, Maps w/ Layers + Cnstr Layer.
    Read the Changelog for more information.

---
+++ Prototyping/Aaron Prototype 1.2/Prototyping/Objects.h
@@ -0,0 +1,38 @@
+#include "Sprite.h"
+
+class Objects : public Sprite
+{
+public:
+	Objects(std::string filename, int width, int height);
+	~Objects(void);
+
+	bool human;
+	virtual void update();
+};
+
+
+/* Character Classes */
+class Characters : public Objects
+{
+public:
+	Characters::Characters(std::string filename, int width, int height);
+	~Characters(void);
+
+	virtual void update();
+
+};
+
+class MainCharacter : public Characters
+{
+public:
+	MainCharacter::MainCharacter(std::string filename, int width, int height);
+	~MainCharacter(void);
+
+	float speedX; // Character's move speed in pixels. X (left or right)
+	float speedY; // Character's move speed in pixels. Y (up or down)
+
+	virtual void update();
+	void movementGo(unsigned char key); //Keyboard down
+	void movementStop(unsigned char key); //Keyboard Up
+	void movement();
+};
