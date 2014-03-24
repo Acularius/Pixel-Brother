a5b007920c5873f02ad30b617bbcb4912174949f
warmarine <bombrock5@yahoo.com>
2014-03-24 18:15:21 -0400

    Switch case

--- Prototyping/Eric's key prototype 0.1/test1/Project1/Project1/Keybinding.cpp
+++ Prototyping/Eric's key prototype 0.1/test1/Project1/Project1/Keybinding.cpp
@@ -21,7 +21,35 @@ void keybinding(char upkey, char leftkey, char downkey, char rightkey, int optio
 	cout<<"3.downkey "<<downkey<<endl;
 	cout<<"4.rightkey "<<rightkey<<endl;
 	cout<<"5.set defaults"<<endl;
-	cin >> optionnum; 
+	cin >> optionnum;
+
+	switch(optionnum)
+	{
+	case 1:
+		cout<<"Set key for up"<<endl;
+		cin>>upkey;
+		cout<<"Upkey is now: "<< upkey<<endl;
+		break;
+	case 2:
+		cout<<"Set key for left"<<endl;
+		cin>>leftkey;
+		cout<<"Leftkey is now: "<< leftkey<<endl;
+
+		break;
+	case 3:
+		cout<<"Set key for down"<<endl;
+		cin>>downkey;
+		cout<<"Downkey is now: "<< downkey<<endl;
+		break;
+	case 4:
+		cout<<"Set key for right"<<endl;
+		cin>>rightkey;
+		cout<<"Rightkey is now: "<< rightkey<<endl;
+		break;
+	case 5:
+		cout<<"Keys stay the same"<<endl;
+		break;
+
 
 }
 
